0362526497ec7ae2d6a1d2b9dcb928ceb14d8d1c
Dinko <dinko.dugec@gmail.com>
2022-12-25 00:36:57 +0100

    naviagtion highlight and static pages

---
+++ resources/views/info.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Info</div>
+
+                <div class="card-body">
+                 Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                  Est, corrupti itaque sint ipsam eum dolorum explicabo, autem possimus ipsum,
+                   porro numquam ullam eos asperiores dolor veritatis labore sit provident.
+                    Veritatis!
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
---
+++ resources/views/starting_page.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Starting Page</div>
+
+                <div class="card-body">
+               Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam,
+               culpa laborum dolorum facilis hic similique perspiciatis quis tempore
+               perferendis atque exercitationem deleniti dolorem reprehenderit veniam aliquam?
+                Beatae unde necessitatibus maxime.
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
