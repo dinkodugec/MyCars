3c2424083dd27f56a2e3cf51bc41ab62498d290f
Dinko <dinko.dugec@gmail.com>
2022-12-30 21:23:24 +0100

    make a seeder for tags

--- database/seeds/TagSeeder.php
+++ database/seeds/TagSeeder.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Database\Seeder;
 
+use App\Tag;
+
 class TagSeeder extends Seeder
 {
     /**
@@ -11,6 +13,26 @@ class TagSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $tags = [
+            'Sports' => 'primary', // blue
+            'Relaxation' => 'secondary', // grey
+            'Fun' => 'warning', // yellow
+            'Electric' => 'success', // green
+            'Hybrid' => 'light', // white grey
+            'SUV' => 'info', // turquoise
+            'Van' => 'danger', // red
+            'OFF road' => 'dark' // black-white
+        ];
+
+        foreach ($tags as $key => $value) {
+            $tag = new Tag(
+                [
+                    'name' => $key,
+                    'slug' => $value
+                ]
+            );
+            $tag->save();
+        }
+
     }
 }
