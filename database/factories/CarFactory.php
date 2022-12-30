c956e24666e32734661d6d4aff82c56afe73f529
Dinko <dinko.dugec@gmail.com>
2022-12-30 22:47:45 +0100

    use each() to seed the linked database tables

--- database/factories/CarFactory.php
+++ database/factories/CarFactory.php
@@ -7,6 +7,8 @@ use Faker\Generator as Faker;
 
 $factory->define(Car::class, function (Faker $faker) {
     return [
-        //
+        'manufacturer' => $faker->realText(30),
+        'name' => $faker->realText(30),
+        'description' => $faker->realText(),
     ];
 });
--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB; // it allows to insert data in DB
 
 class UserSeeder extends Seeder
 {
@@ -11,6 +12,32 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        factory(App\User::class, 100)->create();
+        factory(App\User::class, 100)->create()
+
+
+        ->each(function ($user){  //each user, who is argument in this closure and creating in that moment
+            factory(App\Car::class, rand(1,8))->create(   // in this closure(anonymus function) we call factory,for each user we want to connect random cars, between 1-8
+                [
+                    'user_id' => $user->id
+                ]
+            )
+
+        ->each(function ($car){
+                $tag_ids = range(1,8);
+                shuffle($tag_ids);
+                $assignments = array_slice($tag_ids, 0, rand(0,8)); // eg 5,2,8
+                foreach($assignments as $tag_id){
+                    DB::table('car_tag')
+                        ->insert(  //in DB
+                            [
+                                'car_id' => $car->id,
+                                'tag_id' => $tag_id,
+                                'created_at' => Now(),
+                                'updated_at' => Now()
+                            ]
+                        );
+                }
+            });
+        });
     }
 }
