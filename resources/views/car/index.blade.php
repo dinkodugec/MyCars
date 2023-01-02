4f2e55e65906a5a9ab0897af10d9b25a82a86ac2
Dinko <dinko.dugec@gmail.com>
2023-01-02 21:49:46 +0100

    prepare frontend for images

--- resources/views/car/index.blade.php
+++ resources/views/car/index.blade.php
@@ -1,63 +1,61 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-11">
-            <div class="card">
-                @isset($filter)
-                <div class="card-header">Filtered Cars by <span>{{ $filter->name }}</span>
-                    <span class="float-right"><a href="/car">Show all Cars</a></span></div>
-                @else
-                <div class="card-header">All the Cars</div>
-                @endisset
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-11">
+                <div class="card">
 
-                <div class="card-body">
-                    <ul class="list-group">
-                        @foreach($cars as $car)
-                        <li class="list-group-item">
-                          <a title="Show details" href="/car/{{ $car->id }}">  {{ $car->name }}</a>
+                    @isset($filter)
+                        <div class="card-header">Filtered hobbies by
+                            <span style="font-size: 130%;" class="badge badge-{{ $filter->style }}">{{ $filter->name }}</span>
+                            <span class="float-right"><a href="/car">Show all CARS</a></span>
+                        </div>
+                    @else
+                        <div class="card-header">All the Cars</div>
+                    @endisset
 
-                           @auth
-                              <a class="btn btn-sm btn-light ml-2"  href="/car/{{ $car->id }}/edit">Edit a Car</a>
-                            @endauth
-
-                            <span class="mx-2">Posted by: <a href="user/{{ $car->user->id }}">{{ $car->user->name }}</a>  ({{ $car->user->cars->count() }} Cars)</span>
-
-                            @auth
-                                <form class="float-right" style="display: inline" action="/car/{{ $car->id }}" method="post">
-                                    @csrf
-                                @method("DELETE")
-                                    <input class="btn btn-sm btn-outline-danger" type="submit" value="Delete">
-                                </form>
-
-                           @endauth
-
-
-                         <span class=" float-right mx-2">{{ $car->created_at->diffForHumans() }}</span>
-
-                         <br>
-                                @foreach($car->tags as $tag)
-                                    <a href="/car/tag/{{ $tag->id }}"><span class="badge badge-{{ $tag->style }}">{{ $tag->name }}</span></a>
-                                @endforeach
-                        </li>
-                    @endforeach
-                    </ul>
+                    <div class="card-body">
+                        <ul class="list-group">
+                            @foreach($cars as $car)
+                                <li class="list-group-item">
+                                    <a title="Show Details" href="/car/{{ $car->id }}">
+                                        <img src="/img/thumb_landscape.jpg" alt="thumb">
+                                        {{ $car->name }}
+                                    </a>
+                                    @auth
+                                    <a class="btn btn-sm btn-light ml-2" href="/car/{{ $car->id }}/edit"><i class="fas fa-edit"></i> Edit CAr</a>
+                                    @endauth
+                                    <span class="mx-2">Posted by: <a href="/user/{{ $car->user->id }}">{{ $car->user->name }} ({{ $car->user->cars->count() }} Cars)</a>
+                                    <a href="/user/{{ $car->user->id }}"><img class="rounded" src="/img/thumb_portrait.jpg"></a>
+                                    </span>
+                                    @auth
+                                    <form class="float-right" style="display: inline" action="/car/{{ $car->id }}" method="post">
+                                        @csrf
+                                        @method("DELETE")
+                                        <input class="btn btn-sm btn-outline-danger" type="submit" value="Delete">
+                                    </form>
+                                    @endauth
+                                    <span class="float-right mx-2">{{ $car->created_at->diffForHumans() }}</span>
+                                    <br>
+                                    @foreach($car->tags as $tag)
+                                        <a href="/car/tag/{{ $tag->id }}"><span class="badge badge-{{ $tag->style }}">{{ $tag->name }}</span></a>
+                                    @endforeach
+                                </li>
+                            @endforeach
+                        </ul>
+                    </div>
                 </div>
-            </div>
 
-            <div class="mt-3">
-                {{ $cars->links() }}
+                <div class="mt-3">
+                    {{ $cars->links() }}
+                </div>
+                @auth
+                <div class="mt-2">
+                    <a class="btn btn-success btn-sm" href="/car/create"><i class="fas fa-plus-circle"></i> Create new CAR</a>
+                </div>
+                @endauth
             </div>
         </div>
-        @auth
-            <div class="mt-4">
-                <a class="btn btn-success btn-sm" href="/car/create"><i class="fas fa-plus-circle"></i> Create new Car</a>
-            </div>
-        @endauth
-
     </div>
-</div>
 @endsection
-
-
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,58 +5,60 @@
     <div class="row justify-content-center">
         <div class="col-md-11">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+                <div class="card-header">Dashboard</div>
 
                 <div class="card-body">
-                    <h2>Hello {{ auth()->user()->name }}</h2>
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
+                    <div class="row">
+                        <div class="col-md-9">
+                            <h2>Hello {{ auth()->user()->name }}</h2>
+                            <h5>Your Motto</h5>
+                            <p><p>{{ auth()->user()->motto ?? '' }}</p></p>
+                            <h5>Your "About Me" -Text</h5>
+                            <p><p>{{ auth()->user()->about_me ?? '' }}</p></p>
                         </div>
-                    @endif
+                        <div class="col-md-3">
+                            <img class="img-thumbnail" src="/img/300x400.jpg" alt="{{ auth()->user()->name }}">
+                        </div>
+                    </div>
+
+
 
                     @isset($cars)
-                    @if ($cars->count() > 0)
-                        <h3>Your Cars</h3>
-                    @endif
-                        <ul class="list-group">
+                        @if($cars->count() > 0)
+                        <h3>Your Hobbies:</h3>
+                        @endif
+                    <ul class="list-group">
                         @foreach($cars as $car)
-                        <li class="list-group-item">
-                          <a title="Show details" href="/car/{{ $car->id }}">  {{ $car->name }}</a>
+                            <li class="list-group-item">
+                                <a title="Show Details" href="/car/{{ $car->id }}">
+                                    <img src="/img/thumb_landscape.jpg" alt="thumb"></a>
+                                    {{ $car->name }}
+                                </a>
+                                @auth
+                                    <a class="btn btn-sm btn-light ml-2" href="/car/{{ $car->id }}/edit"><i class="fas fa-edit"></i> Edit CAR</a>
+                                @endauth
 
-                           @auth
-                              <a class="btn btn-sm btn-light ml-2"  href="/car/{{ $car->id }}/edit">Edit a Car</a>
-                            @endauth
-
-
-
-                            @auth
-                                <form class="float-right" style="display: inline" action="/car/{{ $car->id }}" method="post">
-                                    @csrf
-                                @method("DELETE")
-                                    <input class="btn btn-sm btn-outline-danger" type="submit" value="Delete">
-                                </form>
-
-                           @endauth
-
-
-                         <span class=" float-right mx-2">{{ $car->created_at->diffForHumans() }}</span>
-
-                         <br>
+                                @auth
+                                    <form class="float-right" style="display: inline" action="/car/{{ $car->id }}" method="post">
+                                        @csrf
+                                        @method("DELETE")
+                                        <input class="btn btn-sm btn-outline-danger" type="submit" value="Delete">
+                                    </form>
+                                @endauth
+                                <span class="float-right mx-2">{{ $car->created_at->diffForHumans() }}</span>
+                                <br>
                                 @foreach($car->tags as $tag)
                                     <a href="/car/tag/{{ $tag->id }}"><span class="badge badge-{{ $tag->style }}">{{ $tag->name }}</span></a>
                                 @endforeach
-                        </li>
-                    @endforeach
+                            </li>
+                        @endforeach
                     </ul>
                     @endisset
 
-                    <a class="btn btn-success btn-sm mt5" href="/car/create"><i class="fas fa-plus-circle"></i> Create new CAR</a>
+                    <a class="btn btn-success btn-sm" href="/hobby/create"><i class="fas fa-plus-circle"></i> Create new Hobby</a>
                 </div>
             </div>
         </div>
     </div>
 </div>
 @endsection
-
-
